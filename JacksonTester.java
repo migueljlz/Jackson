f432edc1a5532af2202ed0fd36ad4de145193c73
Miguel Jaramillo <miguel.jaramillo@neoris.com>
2022-02-25 13:52:36 -0600

    Jackson tree model implemented

--- JacksonTester.java
+++ JacksonTester.java
@@ -1,74 +1,41 @@
-import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Iterator;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JacksonTester {
    public static void main(String args[]){
-      JacksonTester tester = new JacksonTester();
-         try {
-            ObjectMapper mapper = new ObjectMapper();
 
-            Map<String,Object> studentDataMap = new HashMap<String,Object>(); 
-            int[] marks = {1,2,3};
+      try {
+         ObjectMapper mapper = new ObjectMapper();
+         String jsonString = "{\"name\":\"Mahesh Kumar\",  \"age\":21,\"verified\":false,\"marks\": [100,90,85]}";
+         JsonNode rootNode = mapper.readTree(jsonString);
 
-            Student student = new Student();
-            student.setAge(10);
-            student.setName("Mahesh");
-            // JAVA Object
-            studentDataMap.put("student", student);
-            // JAVA String
-            studentDataMap.put("name", "Mahesh Kumar");   		
-            // JAVA Boolean
-            studentDataMap.put("verified", Boolean.FALSE);
-            // Array
-            studentDataMap.put("marks", marks);
+         JsonNode nameNode = rootNode.path("name");
+         System.out.println("Name: "+ nameNode.textValue());
 
-            mapper.writeValue(new File("student.json"), studentDataMap);
-            //result student.json
-			//{ 
-            //   "student":{"name":"Mahesh","age":10},
-            //   "marks":[1,2,3],
-            //   "verified":false,
-            //   "name":"Mahesh Kumar"
-            //}
-            studentDataMap = mapper.readValue(new File("student.json"), Map.class);
+         JsonNode ageNode = rootNode.path("age");
+         System.out.println("Age: " + ageNode.intValue());
 
-            System.out.println(studentDataMap.get("student"));
-            System.out.println(studentDataMap.get("name"));
-            System.out.println(studentDataMap.get("verified"));
-            System.out.println(studentDataMap.get("marks"));
-      } catch (JsonParseException e) {
-         e.printStackTrace();
-      } catch (JsonMappingException e) {
-         e.printStackTrace();
-      } catch (IOException e) {
-            e.printStackTrace();
+         JsonNode verifiedNode = rootNode.path("verified");
+         System.out.println("Verified: " + (verifiedNode.booleanValue() ? "Yes":"No"));
+
+         JsonNode marksNode = rootNode.path("marks");
+         Iterator<JsonNode> iterator = marksNode.elements();
+         System.out.print("Marks: [ ");
+
+         while (iterator.hasNext()) {
+            JsonNode marks = iterator.next();
+            System.out.print(marks.intValue() + " "); 
+         }
+
+         System.out.println("]");
       }
+      catch (JsonParseException e) { e.printStackTrace(); }
+      catch (JsonMappingException e) { e.printStackTrace(); }
+      catch (IOException e) { e.printStackTrace(); }
    }
-}
-
-class Student {
-   private String name;
-   private int age;
-   public Student(){}
-   public String getName() {
-      return name;
-   }
-   public void setName(String name) {
-      this.name = name;
-   }
-   public int getAge() {
-      return age;
-   }
-   public void setAge(int age) {
-      this.age = age;
-   }
-   public String toString(){
-      return "Student [ name: "+name+", age: "+ age+ " ]";
-   }	
 }
